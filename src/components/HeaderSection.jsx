279f10e7610c95af476502f47d3e770ffa26255f
Lovish-Singlaa <165283544+Lovish-Singlaa@users.noreply.github.com>
2024-11-12 16:46:31 +0530

    Update HeaderSection.jsx

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -20,7 +20,7 @@ const HeaderSection = () => {
                 <h2 className='text-3xl font-bold text-center'>Find your New Job Easiest Way <br /> <span className='text-[#ff4f6c]'>With CareerBridge</span></h2>
             </div>
             <div className='flex md:w-[50%] items-center mx-auto my-8 h-10 relative shadow-lg border border-gray-200 rounded-full'>
-            <input onChange={(e)=>setQuery(e.target.value)} type="text" placeholder="Search your jobs" className=' w-full h-full py-2 px-3 rounded-full border border-gray-300 focus:outline-none focus:ring-1 focus:ring-red-400'/>
+            <input onChange={(e)=>setQuery(e.target.value)} type="text" placeholder="Search your jobs" className=' w-full h-full py-2 px-3 rounded-full border border-gray-300 text-gray-700 focus:outline-none focus:ring-1 focus:ring-red-400'/>
             <button onClick={searchHandler} className='bg-red-500 h-full absolute right-0 border px-3 rounded-e-full'><Search className='text-white'/></button>
             </div>
         </div>
